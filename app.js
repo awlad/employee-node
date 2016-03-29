0d3246ac0edf9fb33eddd3bcc4604c6a96ace91b
awlad <awladliton@gmail.com>
2016-03-29 23:28:33 +0600

    added some style with latest express version changes.

--- app.js
+++ app.js
@@ -6,32 +6,45 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
-//extra module
-var http = require('http');
-var connection  = require('express-myconnection');
-var mysql = require('mysql');
-
 var app = express();
 
-app.set('port', process.env.PORT || 5000);
 
+var users = require('./routes/users');
+var employees = require('./routes/employees');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
+
+//connection manager in Express
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+//app.use(
+//
+//    connection(mysql,{
+//
+//      host: 'localhost',
+//      user: 'root',
+//      password : 'root',
+//      port : 3306, //port mysql
+//      database:'nodejs'
+//    },'request')
+//);
+
+
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use('/', routes);
+app.use('/users', users);
+app.use('/employees', employees);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -64,65 +77,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.use(
+app.listen(8081);
 
-    connection(mysql,{
-
-      host: 'localhost',
-      user: 'root',
-      password : 'root',
-      port : 3306, //port mysql
-      database:'nodejs'
-    },'request')
-);
-
-//app.use('/', routes);
-app.use('/users', users);
-
-app.get('/', routes.index);
-app.get('/employees', employees.list);//route add customer, get n post
-app.get('/employees/add', employees.add);
-app.post('/employees/add', employees.save);//route delete customer
-app.get('/employees/delete/:id', employees.delete);//edit customer route , get n post
-app.get('/employees/edit/:id', employees.edit);
-app.post('/employees/edit/:id',employees.update);
-app.use(app.router);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-//module.exports = app;
-
-
-//var express = require('express');
-//var routes = require('./routes');
-//var http = require('http');
-//var path = require('path');
-////load customers route
-//var employees = require('./routes/employees');
-//var app = express();
-//var connection  = require('express-myconnection');
-//var mysql = require('mysql');
-//
-//// all environments
-//app.set('port', process.env.PORT || 5000);
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-//
-//app.use(express.logger('dev'));
-//app.use(express.json());
-//app.use(express.urlencoded());
-//app.use(express.methodOverride());
-//app.use(express.static(path.join(__dirname, 'public')));
-//// development only
-//if ('development' == app.get('env')) {
-//  app.use(express.errorHandler());
-//}
-/*------------------------------------------
- connection peer, register as middleware
- type koneksi : single,pool and request
- -------------------------------------------*/
-
-
-
+module.exports = app;
--- routes/employees.js
+++ routes/employees.js
@@ -1,46 +1,91 @@
+var express = require('express');
+var router = express.Router();
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'nodejs'
+});
+
+/* GET Employee listing. */
+router.get('/', function(req, res, next) {
+    //res.send('respond with a resource Employee');
+    connection.connect(function (err){
+        console.log('Connection with the officeball MySQL database openned...');
+        if (err) return callback(new Error('Failed to connect'), null);
+        // if no error, you can do things now.
+        connection.query('SELECT * FROM employees',function(err,rows)     {
+
+            if(err)
+                console.log("Error Selecting : %s ",err );
+
+            res.render('employees/list', { page_title:"Employees | NodeJs", data:rows});
+
+        });
+
+
+    });
+
+
+});
+
+/* GET edit employee. */
+router.get('/employees/:id', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+/* PUT update listing. */
+router.put('/employees/:id', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+/* Post Create Employee */
+router.post('/employees', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+
+module.exports = router;
+
 
 exports.list = function(req, res){
-    req.getConnection(function(err, connection){
+    req.getConnection(function(err,connection){
 
-        connection.query('SELECT * FROM employees',function(err, rows)     {
+        connection.query('SELECT * FROM customer',function(err,rows)     {
 
-            if(err) {
-                console.log("Error Selecting : %s ", err );
-            }
+            if(err)
+                console.log("Error Selecting : %s ",err );
 
-            res.render('employees/list', { page_title: "Employees", data: rows });
+            res.render('customers',{page_title:"Customers - Node.js",data:rows});
 
         });
 
     });
 
 };
-
 exports.add = function(req, res){
-    res.render('employees/add', { page_title:"New Employee" });
+    res.render('add_customer',{page_title:"Add Customers-Node.js"});
 };
-
 exports.edit = function(req, res){
 
     var id = req.params.id;
 
     req.getConnection(function(err,connection){
 
-        connection.query('SELECT * FROM employees WHERE id = ?',[id],function(err, row)
+        connection.query('SELECT * FROM customer WHERE id = ?',[id],function(err,rows)
         {
 
-            if(err) {
-
+            if(err)
                 console.log("Error Selecting : %s ",err );
-            }
 
-            res.render('employees/edit',{ page_title:"Edit Employee", data:row });
+            res.render('edit_customer',{page_title:"Edit Customers - Node.js",data:rows});
 
         });
 
     });
 };
-
+/*Save the customer*/
 exports.save = function(req,res){
 
     var input = JSON.parse(JSON.stringify(req.body));
@@ -49,29 +94,28 @@ exports.save = function(req,res){
 
         var data = {
 
-            name     : input.name,
-            address  : input.address,
-            email    : input.email,
-            position : input.position
+            name    : input.name,
+            address : input.address,
+            email   : input.email,
+            phone   : input.phone
 
         };
 
-        var query = connection.query("INSERT INTO employees set ? ",data, function(err, rows)
+        var query = connection.query("INSERT INTO customer set ? ",data, function(err, rows)
         {
 
             if (err)
                 console.log("Error inserting : %s ",err );
 
-            res.redirect('/employees');
+            res.redirect('/customers');
 
         });
 
         // console.log(query.sql); get raw query
 
     });
-};
-
-exports.update = function(req,res){
+};/*Save edited customer*/
+exports.save_edit = function(req,res){
 
     var input = JSON.parse(JSON.stringify(req.body));
     var id = req.params.id;
@@ -87,26 +131,26 @@ exports.update = function(req,res){
 
         };
 
-        connection.query("UPDATE employees set ? WHERE id = ? ",[data,id], function(err, rows)
+        connection.query("UPDATE customer set ? WHERE id = ? ",[data,id], function(err, rows)
         {
 
             if (err)
                 console.log("Error Updating : %s ",err );
 
-            res.redirect('/employees');
+            res.redirect('/customers');
 
         });
 
     });
 };
 
-exports.delete = function(req,res){
+exports.delete_customer = function(req,res){
 
     var id = req.params.id;
 
     req.getConnection(function (err, connection) {
 
-        connection.query("DELETE FROM employees  WHERE id = ? ",[id], function(err, rows)
+        connection.query("DELETE FROM customer  WHERE id = ? ",[id], function(err, rows)
         {
 
             if(err)
